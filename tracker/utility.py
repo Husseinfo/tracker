c63c14b4cb5a47ce860b2cef129a118da6a4ab09
Hussein <husseinfo@mail.com>
2017-05-11 20:57:47 +0300

    Designing & Styling

--- tracker/utility.py
+++ tracker/utility.py
@@ -7,6 +7,8 @@ def time_spent(sec):
     lm = datetime.datetime.now() - datetime.datetime.fromtimestamp(sec)
     if lm.days > 0:
         return str(lm.days) + 'd'
-    if lm.seconds / 3600 > 0:
+    if int(lm.seconds / 3600) > 0:
         return str(int(lm.seconds / 3600)) + 'h'
-    return str(int(lm.seconds / 60)) + 'm'
+    if int(lm.seconds / 60) > 0:
+        return str(int(lm.seconds / 60)) + 'm'
+    return str(int(lm.seconds)) + 's'
