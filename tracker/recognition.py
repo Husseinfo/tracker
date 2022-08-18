6696bb6a3705b53e74fc837eddd1ccda91543a54
Hussein Srour <husseinfo@mail.com>
2022-08-18 22:14:18 +0200

    testable recog module

--- tracker/recognition.py
+++ tracker/recognition.py
@@ -5,23 +5,23 @@ from pickle import dump, load
 from face_recognition import face_locations, face_encodings, load_image_file
 from sklearn.neighbors import KNeighborsClassifier
 
-from . import model_filename
+from . import model_filename, photos_path
 
 knn_clf: KNeighborsClassifier | None = None
 
 
-def get_nbr_photos() -> int:
+def get_nbr_photos(photos=photos_path) -> int:
     try:
-        return len(listdir('static/photos'))
+        return len(listdir(photos))
     except Exception as e:
         print(e)
         return 0
 
 
-def get_dataset():
+def get_dataset(photos=photos_path):
     dataset = [[], []]
-    for photo in listdir('static/photos'):
-        photo_file = load_image_file(f'static/photos/{photo}')
+    for photo in listdir(photos):
+        photo_file = load_image_file(f'{photos}/{photo}')
         locations = face_locations(photo_file)
         if len(locations) != 1:
             print(f'Photo should have one exact face, skipping photo at {photo}')
@@ -31,15 +31,15 @@ def get_dataset():
     return dataset
 
 
-def get_classifier():
+def get_classifier(model=model_filename):
     global knn_clf
     if not knn_clf:
-        with open(model_filename, 'rb') as f:
+        with open(model, 'rb') as f:
             knn_clf = load(f)
     return knn_clf
 
 
-def train():
+def train(model=model_filename):
     global knn_clf
     encodings, labels = get_dataset()
     n_neighbors = int(round(sqrt(len(encodings))))
@@ -49,7 +49,7 @@ def train():
     knn_clf.fit(encodings, labels)
 
     # Save the trained KNN classifier
-    with open('static/model', 'wb') as f:
+    with open(model, 'wb') as f:
         dump(knn_clf, f)
 
 
